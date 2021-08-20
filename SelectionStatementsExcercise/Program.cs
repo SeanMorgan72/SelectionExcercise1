f6bcceeb277d6f4ec5c6b85545901c0c46ee9dae
Sean Morgan <scm0323@att.net>
2021-08-20 15:24:49 -0500

    excercise one if-else

---
+++ SelectionStatementsExcercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatementsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ranNum = new Random();
+            var favNum = ranNum.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number.");
+            var userIn = int.Parse(Console.ReadLine());
+
+            if(userIn < favNum)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if(userIn > favNum)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind!");
+            }
+        }
+    }
+}
